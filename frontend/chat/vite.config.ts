8b62a1a65391d951a6feecda36112692d56f680d
Eslam-M <eslam_fcih@hotmail.com>
2023-07-07 01:04:16 +0300

    vite as a library

--- frontend/chat/vite.config.ts
+++ frontend/chat/vite.config.ts
@@ -1,7 +1,16 @@
+// vite.config.js
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  define: { 'process.env.NODE_ENV': '"production"' },
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/main.tsx'),
+      name: 'MyLib',
+      // the proper extensions will be added
+      fileName: 'my-lib',
+    },
+  },
 })
